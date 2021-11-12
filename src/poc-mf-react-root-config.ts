1d19dd8527948402312fa07f45ef9d8e9143ea28
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2021-11-12 17:55:54 -0300

    chore: add fonts

--- src/poc-mf-react-root-config.ts
+++ src/poc-mf-react-root-config.ts
@@ -1,12 +1,13 @@
-import { registerApplication, start } from "single-spa";
+import { registerApplication, start } from 'single-spa';
 import {
   constructApplications,
   constructRoutes,
   constructLayoutEngine,
-} from "single-spa-layout";
-import microfrontendLayout from "./microfrontend-layout.html";
+} from 'single-spa-layout';
+import microfrontendLayout from './microfrontend-layout.html';
 
 const routes = constructRoutes(microfrontendLayout);
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
